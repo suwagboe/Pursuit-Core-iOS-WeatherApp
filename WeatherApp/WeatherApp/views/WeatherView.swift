da65676173669fc264c1c5468c459704b0cbd1bb
uwagboe <sm.uwagboe@gmail.com>
2020-01-31 15:52:46 -0500

    i have access to the view controller after I set up the window inside of the scene delegate

---
+++ WeatherApp/WeatherApp/views/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Pursuit on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
