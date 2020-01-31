0b2537bae4cde4e71f85f91387cd1717467e1f2f
uwagboe <sm.uwagboe@gmail.com>
2020-01-31 18:53:05 -0500

    made the weather model

---
+++ WeatherApp/WeatherApp/Model/WeatherSearch.swift
@@ -0,0 +1,28 @@
+//
+//  WeatherSearch.swift
+//  WeatherApp
+//
+//  Created by Pursuit on 1/31/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherSearch: Codable {
+    let latitude: Double
+    let longitude: Double
+    let timeZone: String
+    let daily: [Daily]
+    
+}
+
+struct Daily: Codable {
+    let icon: String
+    let data: [Data]
+}
+
+struct Data: Codable {
+    let Summary: String
+    let temperatureHigh: Double
+    let temperatureLow: Double
+}
