46b15de34c6490af3d45a7b3aea563e2249e4d06
uwagboe <sm.uwagboe@gmail.com>
2020-02-03 20:08:37 -0500

    made the constraints for the two labels and the fixed the collection view and textField button

--- WeatherApp/WeatherApp/views/WeatherView.swift
+++ WeatherApp/WeatherApp/views/WeatherView.swift
@@ -22,9 +22,27 @@ class WeatherView: UIView {
     
     public lazy var textField: UITextField = {
         let text = UITextField()
-        text.placeholder = "please input a zipcode here"
+        text.placeholder = "zipcode"
         text.backgroundColor = .blue
         return text
+    }()
+    
+    public lazy var textFieldLabel: UILabel = {
+        
+        let label = UILabel()
+        
+        label.text = "Please enter a zipcode below"
+        label.backgroundColor = .green
+        
+        return label
+    }()
+    
+    public lazy var weatherLabel: UILabel = {
+        let label = UILabel()
+        label.text = "The weather this area is..."
+        
+        label.textColor = .green
+        return label
     }()
     
     override init(frame: CGRect){
@@ -40,7 +58,37 @@ class WeatherView: UIView {
     private func commonInit(){
         setUpCollectionView()
         setUpTextField()
+        setUptextFieldLabel()
+        setUpWeatherLabel()
     }
+    
+    private func setUptextFieldLabel() {
+        addSubview(textFieldLabel)
+        
+        textFieldLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            textFieldLabel.topAnchor.constraint(equalTo: weatherCollection.bottomAnchor, constant: 20),
+            textFieldLabel.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor)
+            
+        
+        ])
+    
+    }
+    
+    private func setUpWeatherLabel(){
+        addSubview(weatherLabel)
+        
+        weatherLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            weatherLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+                //weatherLabel.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 60),
+             //  weatherLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -45)
+            weatherLabel.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor)
+        ])
+    }
+    
     private func setUpTextField(){
         
         addSubview(textField)
@@ -48,7 +96,7 @@ class WeatherView: UIView {
         
         NSLayoutConstraint.activate([
             // will this place it 40 down from the top...
-            textField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -600 ),
+            textField.topAnchor.constraint(equalTo: weatherCollection.bottomAnchor, constant: 60),
          //   textField.widthAnchor.constraint(equalTo: widthAnchor, constant: 0.5)
             textField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 100),
             textField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -100),
@@ -62,11 +110,9 @@ class WeatherView: UIView {
         addSubview(weatherCollection)
         
         weatherCollection.translatesAutoresizingMaskIntoConstraints = false
-        
-        //weatherCollection.anchor(top: weatherCollection.safeAreaLayoutGuide.topAnchor, leading: weatherCollection.safeAreaLayoutGuide.leadingAnchor, bottom: weatherCollection.safeAreaLayoutGuide.bottomAnchor, trailing: weatherCollection.safeAreaLayoutGuide.trailingAnchor)
-        
+
         NSLayoutConstraint.activate([
-            weatherCollection.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            weatherCollection.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 70),
             weatherCollection.leadingAnchor.constraint(equalTo: leadingAnchor),
             weatherCollection.trailingAnchor.constraint(equalTo: trailingAnchor),
             weatherCollection.heightAnchor.constraint(equalTo: heightAnchor, constant: -750)
